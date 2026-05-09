f57129c732467098daa8be6ebf273af7030b7578
Valery Piashchynski <piashchynski.valery@gmail.com>
2026-05-09 08:16:05 +0200

    chore(tests): migrate v5 → v6 RR plugin imports
    v5 plugins return *zap.Logger which doesn't satisfy v6 plugins'
    *slog.Logger-based interfaces; endure couldn't resolve init deps.
    Switch tests/ to all-v6 plugins; goridge/v3 and pool v1.1.3 indirects
    drop out via go mod tidy.

--- tests/storage_plugin_test.go
+++ tests/storage_plugin_test.go
@@ -14,14 +14,14 @@ import (
 	"time"
 
 	kvProto "github.com/roadrunner-server/api/v4/build/kv/v1"
-	"github.com/roadrunner-server/config/v5"
+	"github.com/roadrunner-server/config/v6"
 	"github.com/roadrunner-server/endure/v2"
 	goridgeRpc "github.com/roadrunner-server/goridge/v4/pkg/rpc"
-	"github.com/roadrunner-server/kv/v5"
-	"github.com/roadrunner-server/logger/v5"
-	"github.com/roadrunner-server/metrics/v5"
+	"github.com/roadrunner-server/kv/v6"
+	"github.com/roadrunner-server/logger/v6"
+	"github.com/roadrunner-server/metrics/v6"
 	"github.com/roadrunner-server/redis/v6"
-	rpcPlugin "github.com/roadrunner-server/rpc/v5"
+	rpcPlugin "github.com/roadrunner-server/rpc/v6"
 	"github.com/stretchr/testify/assert"
 )
 
